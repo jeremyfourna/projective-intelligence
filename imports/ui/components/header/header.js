668bf2e9a729a698086f4e47586531540d63f86d
dexterneo <jeremyfourna@gmail.com>
2016-10-04 23:03:59 +0200

    Add mobile nav bar

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -2,6 +2,14 @@ import { Template } from 'meteor/templating';
 
 import './header.jade';
 
+Template.header.onRendered(function() {
+	$('.button-collapse').sideNav({
+		menuWidth: 300, // Default is 240
+		edge: 'right', // Choose the horizontal origin
+		closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+	});
+});
+
 Template.header.helpers({
 	isUserAdmin() {
 		return Meteor.user().profile.admin;
